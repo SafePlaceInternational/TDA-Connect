7bb99fbfc51ae9cf9c1be2b2a5ac926c6d8ff4c7
divya-prav <divya.praveen.5215@gmail.com>
2024-12-11 11:48:24 -0600

    Global css configuration Added

---
+++ src/app/client-layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { RefObject, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  
+}
