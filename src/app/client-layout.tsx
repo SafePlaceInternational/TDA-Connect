609946d9d6c0280a35f5d2b01e9f82fcdf2f3c78
divya-prav <divya.praveen.5215@gmail.com>
2024-12-12 12:00:08 -0600

    App layout Added

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -8,5 +8,37 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
+  const containerRef: RefObject<HTMLDivElement> = useRef(null);
+  const [hasOverflow, setHasOverflow] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (containerRef.current) {
+        const hasVerticalOverflow =
+          containerRef.current.scrollHeight > containerRef.current.clientHeight;
+        setHasOverflow(hasVerticalOverflow);
+      }
+    };
+    checkOverflow();
+    window.addEventListener("resize", checkOverflow);
+
+    return () => window.removeEventListener("resize", checkOverflow);
+  }, [pathname]);
+  return (
+    <div className="flex flex-col max-h-screen h-screen">
+      <main
+        id="client-layout-inner1"
+        className="flex-grow flex flex-col overflow-scroll"
+        ref={containerRef}
+      >
+        <div
+          id="client-layout-inner2"
+          className="flex-grow flex justify-start px-4"
+        >
+          {children}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { DM_Sans } from "next/font/google";
+import ClientLayout from "./client-layout";
 
 const dmSans = DM_Sans({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -22,7 +23,7 @@ export default function RootLayout({
       <body
         className={`${dmSans.className} my-0 max-w-[640px] justify-center items-center mx-auto shadow-[rgba(100,100,111,0.2)_0px_7px_29px_0px] antialiased`}
       >
-        {children}
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
