26fa371986aecf88632c98b8e0110826ebeb39ac
jeniferXavier <jenifer.xavierraj@gmail.com>
2024-12-16 11:16:28 -0500

    Created add post

--- src/components/home-page/page.tsx
+++ src/components/home-page/page.tsx
@@ -1,8 +1,10 @@
 import ViewPost from "@/components/view-post/view-post";
 
+import AddPost from "@/components/add-post/add-post";
 export default function Home() {
   return (
     <div className="w-full">
+      <AddPost></AddPost>
       <ViewPost />
     </div>
   );
