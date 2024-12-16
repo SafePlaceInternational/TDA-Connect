4bd33ede5a37b90733ecf7638f49f7ca103bb3e6
divya-prav <divya.praveen.5215@gmail.com>
2024-12-16 13:05:29 -0600

    bg Color updated

--- src/components/add-post/post-sort.tsx
+++ src/components/add-post/post-sort.tsx
@@ -4,18 +4,18 @@ export default function PostSort(){
     return(
         <div className="flex w-full px-4 justify-center items-center">
             <div className="flex w-[40%] py-4 flex-col justify-center items-start gap-2 shrink-0 self-stretch">
-                <hr className="border-black border-[1px] my-3 px-3 w-full"/>
+                <hr className="border-primary-500 border-[1px] my-3 px-3 w-full"/>
             </div>
             <div className="w-[60%]">
             <select
                     className="block w-full pl-3 pr-8 py-2 bg-[url('/add-post/up-down-arrow.svg')] bg-no-repeat bg-right
-                    text-base bg-black text-white
+                    text-base bg-primary-500 text-white
                     focus:outline-none focus:ring-2 focus:ring-indigo-500
                     focus:border-green-500 sm:text-sm rounded-full shadow-sm 
                     appearance-none"
                 >
                     <option>Sort by: Most recent</option>
-                    <option>Most relevant</option>
+                    <option>Sort by: Most relevant</option>
                 </select>
             </div>
         </div>
--- src/components/home-page/page.tsx
+++ src/components/home-page/page.tsx
@@ -1,10 +1,10 @@
 import ViewPost from "@/components/view-post/view-post";
 import TryQuickExit from "../try-quick-exit/page";
-
 import AddPost from "@/components/add-post/add-post";
+
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className="w-full mt-4">
       <AddPost/>
       <TryQuickExit/>
       <ViewPost />
