cd985a27cb01e5bde2a000bf2d0f01f1a2d8a48b
divya-prav <divya.praveen.5215@gmail.com>
2025-01-07 21:40:10 -0600

    Google Oauth Added

--- src/components/sign-in/google-signin-button.tsx
+++ src/components/sign-in/google-signin-button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 const GoogleSignInButton = () => (
   <div className="pt-6">
     <button
       className="flex w-full justify-center items-center gap-2 py-4 px-4 rounded-full bg-white border border-black"
-      onClick={() => {/* Add Google Sign In Logic */}}
+      onClick={() => signIn("google")}
       type="button"
     >
       <Image
